7abd168ad9ac2eac0d5e4e500902a651f8f64a21
Brenda Lau <brendalaupg@icloud.com>
2019-08-27 23:03:04 +0800

    Initial Commit

---
+++ Custom.xctemplate/List/___VARIABLE_modelName___Cell.swift
@@ -0,0 +1,7 @@
+//___FILEHEADER___
+
+import Foundation
+import UIKit
+
+class ___VARIABLE_modelName___Cell: UICollectionViewCell {
+}
---
+++ Custom.xctemplate/ListTable/___VARIABLE_viewController___.swift
@@ -0,0 +1,8 @@
+//___FILEHEADER___
+
+import Foundation
+import UIKit
+
+class ___VARIABLE_viewController___: UIViewController {
+    let model: ___VARIABLE_modelName___
+}
