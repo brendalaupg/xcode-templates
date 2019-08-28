1111d9e24407807cd0573a2bbaabf8eceb7f2433
Brenda Lau <brendalaupg@icloud.com>
2019-08-29 00:18:51 +0800

    add template for detail view controller

---
+++ Custom.xctemplate/Detail/___VARIABLE_modelName___DetailView.swift
@@ -0,0 +1,13 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___VARIABLE_modelName___DetailView: UIView {
+    // MARK: - IBOutlets
+    @IBOutlet private weak var titleLabel: UILabel?
+
+    // MARK: - Functions
+    func configure(item: ___VARIABLE_modelName___? = nil) {
+
+    }
+}
--- Custom.xctemplate/Detail/___VARIABLE_viewController___.swift
+++ Custom.xctemplate/Detail/___VARIABLE_viewController___.swift
@@ -1,8 +1,31 @@
 //___FILEHEADER___
 
-import Foundation
 import UIKit
 
 class ___VARIABLE_viewController___: UIViewController {
-    let model: ___VARIABLE_modelName___
+    // MARK: - IBOutlets
+    @IBOutlet private weak var scrollView: UIScrollView?
+    @IBOutlet private weak var itemFormView: ___VARIABLE_modelName___DetailView?
+
+    // MARK: - Variables
+    var item: ___VARIABLE_modelName___?
+
+    // MARK: - View Controller Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.scrollView?.refreshControl = UIRefreshControl()
+        self.scrollView?.refreshControl?.addTarget(self, action: #selector(self.refresh(_:)), for: .valueChanged)
+
+        self.itemFormView?.configure(item: self.item)
+    }
+
+    // MARK: - Functions
+    @IBAction private func refresh(_ sender: Any) {
+        // Query
+    }
+
+    @IBAction private func edit(_ sender: Any) {
+        // Navigate to formVC
+    }
 }
